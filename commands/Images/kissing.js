5f11846434dc2d47f1077b0162785ab5ab5fcd75
protogenposting <86360309+protogenposting@users.noreply.github.com>
2025-10-15 20:12:17 -0400

    fix

--- commands/Images/kissing.js
+++ commands/Images/kissing.js
@@ -25,7 +25,7 @@ module.exports = {
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ ephemeral: false });
 
         const targetUsers = [
             interaction.options.getUser('target1'),
