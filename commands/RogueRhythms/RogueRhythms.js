06e638eb716d309713f62b027bbb06b2f8502556
protogenposting <86360309+protogenposting@users.noreply.github.com>
2025-10-17 16:53:55 -0400

    wip rr data

---
+++ commands/RogueRhythms/RogueRhythms.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+
+const { Jimp, intToRGBA, rgbaToInt } = require('jimp');
+
+const axios = require('axios');
+
+const output = "./output.png"
+
+const dataLink = "http://24.199.91.149:3000/api/dailyScore"
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('rrdailydata')
+        .setDescription('Get data about the current daily!'),
+    async execute(interaction) {
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.deferReply({ ephemeral: false });
+
+        let response = await axios.get(dataLink, {
+            responseType: 'stream'
+        })
+
+        let file = new Jimp({
+            width: image.bitmap.width,
+            height: image.bitmap.height,
+            color: 0xffffffff,
+        });
+
+        await jimp.write(output)
+
+        const attachment = new AttachmentBuilder(output, { name: "file.png" });
+
+        await interaction.editReply(
+            {
+                response,
+                files: [attachment]
+            }
+        );
+
+        fs.unlink(output, () => {})
+    },
+};
